ff9515f86c0103e4f35f692437cf4c52618b0d00
Frederiks <q.frederiks@huizen.nl>
2026-08-13 13:49:01 +0200

    Add Scaler for API Documentation

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,6 @@
 using Backend.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,14 +20,19 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    // Expose the OpenAPI document.
+    // OpenAPI document
     app.MapOpenApi();
 
-    // Expose Swagger UI.
+    // Swagger UI
     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/openapi/v1.json", "Backend API v1");
+        options.SwaggerEndpoint(
+            "/openapi/v1.json",
+            "Backend API v1");
     });
+
+    // Scalar API Reference
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
