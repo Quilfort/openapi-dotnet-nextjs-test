249188f0ff05f8e3724b87d08621b8d52029dfed
Frederiks <q.frederiks@huizen.nl>
2026-08-13 13:44:14 +0200

    Add Swagger UI to project

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -8,17 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
+    // Expose the OpenAPI document.
     app.MapOpenApi();
+
+    // Expose Swagger UI.
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Backend API v1");
+    });
 }
 
 app.UseHttpsRedirection();
